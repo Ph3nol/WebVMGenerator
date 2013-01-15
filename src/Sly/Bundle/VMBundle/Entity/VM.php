38714c242ff3962944c3812e12fe42778bb5e454
Ph3nol <ph3@slynett.com>
2013-01-15 15:33:25 +0100

    VM entity/repository created

---
+++ src/Sly/Bundle/VMBundle/Entity/VM.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Sly\Bundle\VMBundle\Entity;
+
+use Sly\Bundle\VMBundle\Model\VM as BaseVM;
+
+/**
+ * VM entity.
+ *
+ * @uses \Sly\Bundle\VMBundle\Model\VM
+ * @author Cédric Dugat <cedric@dugat.me>
+ */
+class VM extends BaseVM
+{
+}
---
+++ src/Sly/Bundle/VMBundle/Entity/VMRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Sly\Bundle\VMBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * VM repository.
+ */
+class VMRepository extends EntityRepository
+{
+}
