a85f0e59567552fbe35758d07b5d6df21cf285b4
Ph3nol <ph3@slynett.com>
2013-01-15 15:20:47 +0100

    VM model updated

--- src/Sly/Bundle/VMBundle/Model/VM.php
+++ src/Sly/Bundle/VMBundle/Model/VM.php
@@ -17,6 +17,16 @@ class VM
     /**
      * @var array
      */
+    private $web;
+
+    /**
+     * @var array
+     */
+    private $db;
+
+    /**
+     * @var array
+     */
     private $phpModules;
 
     /**
@@ -42,5 +52,105 @@ class VM
         $this->configuration = $vmConfiguration['configuration'];
         $this->phpModules    = $vmConfiguration['phpModules'];
         $this->tools         = $vmConfiguration['tools'];
+    }
+
+    /**
+     * Get Configuration.
+     *
+     * @return array Configuration value
+     */
+    public function getConfiguration()
+    {
+        return $this->configuration;
+    }
+    
+    /**
+     * Set Configuration.
+     *
+     * @param array $configuration Configuration value
+     */
+    public function setConfiguration($configuration)
+    {
+        $this->configuration = $configuration;
+    }
+
+    /**
+     * Get Web.
+     *
+     * @return array Web value
+     */
+    public function getWeb()
+    {
+        return $this->web;
+    }
+    
+    /**
+     * Set Web.
+     *
+     * @param array $web Web value
+     */
+    public function setWeb($web)
+    {
+        $this->web = $web;
+    }
+
+    /**
+     * Get Db.
+     *
+     * @return array Db value
+     */
+    public function getDb()
+    {
+        return $this->db;
+    }
+    
+    /**
+     * Set Db.
+     *
+     * @param array $db Db value
+     */
+    public function setDb($db)
+    {
+        $this->db = $db;
+    }
+
+    /**
+     * Get PhpModules.
+     *
+     * @return array PhpModules value
+     */
+    public function getPhpModules()
+    {
+        return $this->phpModules;
+    }
+    
+    /**
+     * Set PhpModules.
+     *
+     * @param array $phpModules PhpModules value
+     */
+    public function setPhpModules($phpModules)
+    {
+        $this->phpModules = $phpModules;
+    }
+
+    /**
+     * Get Tools.
+     *
+     * @return array Tools value
+     */
+    public function getTools()
+    {
+        return $this->tools;
+    }
+    
+    /**
+     * Set Tools.
+     *
+     * @param array $tools Tools value
+     */
+    public function setTools($tools)
+    {
+        $this->tools = $tools;
     }
 }
